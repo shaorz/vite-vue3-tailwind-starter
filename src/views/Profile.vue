d35c1102d856700be6a7aeb65fca83005f117c6a
Roger SHAO <shaorz@gmail.com>
2022-05-11 23:05:01 +0800

    DYY-0001 refactor new profile component

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+<!--  <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">-->
 
   <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
 
@@ -54,42 +54,9 @@
 
   </div>
 
-  <script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
-  <script src="https://unpkg.com/tippy.js@4"></script>
-  <script>
-    //Init tooltips
-    tippy('.link',{
-      placement: 'bottom'
-    })
-
-    //Toggle mode
-    const toggle = document.querySelector('.js-change-theme');
-    const body = document.querySelector('body');
-    const profile = document.getElementById('profile');
 
 
-    toggle.addEventListener('click', () => {
-
-      if (body.classList.contains('text-gray-900')) {
-        toggle.innerHTML = "☀️";
-        body.classList.remove('text-gray-900');
-        body.classList.add('text-gray-100');
-        profile.classList.remove('bg-white');
-        profile.classList.add('bg-gray-900');
-      } else
-      {
-        toggle.innerHTML = "🌙";
-        body.classList.remove('text-gray-100');
-        body.classList.add('text-gray-900');
-        profile.classList.remove('bg-gray-900');
-        profile.classList.add('bg-white');
-
-      }
-    });
-
-  </script>
-
-  </body>
+<!--  </body>-->
 </template>
 
 <!--<script>-->
