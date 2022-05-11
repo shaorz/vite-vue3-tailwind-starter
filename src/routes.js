2bff31491c3c5d6c2180c9f88a79559462f62624
Roger SHAO <shaorz@gmail.com>
2022-05-11 23:01:24 +0800

    DYY-0001 refactor typo

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
-import Profile from './views/Profile'
+import Profile from './views/Profile.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
